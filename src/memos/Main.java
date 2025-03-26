a961dc079c32b1bdab41a933caffc90a0b5c3645
LeeYouHyeon <941013yhl@gmail.com>
2025-03-26 16:28:12 +0900

    자바 프로젝트 완성

--- src/memos/Main.java
+++ src/memos/Main.java
@@ -1,7 +1,19 @@
 package memos;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("I".compareTo("a"));
+	public static void main(String[] args) throws Exception {
+		BufferedReader labyrinth = new BufferedReader(new FileReader("labyrinth.txt"));
+		while (true) {
+			String line = labyrinth.readLine();
+			if (line == null) {
+				break;
+			}
+			System.out.println(line);
+		}
+		
+		labyrinth.close();
 	}
 }
